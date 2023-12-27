c39f3935c0673c23681fb5371e4971875f13f616
Oskar van Ardenne <ardenne@Oskars-MacBook-Air.local>
2023-12-27 12:32:54 +0100

    converted utilties

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ const arrays = require('./utilities/arrays.js');
 const numbers = require('./utilities/numbers.js');
 const strings = require('./utilities/strings.js');
 
-const numArr = [3, 4, 5, 6];
-const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
-const arrSum = arrays.addArr(numArr);
+const numArr : number[] = [3, 4, 5, 6];
+const wordArr : string[]= ['cat', 'dog', 'rabbit', 'bird'];
+const arrSum : number = arrays.addArr(numArr);
 const mixArr = arrays.concatArr(numArr, wordArr);
-const myNum = '15' % 2;
+const myNum : number = 15 % 2;
 
 // results of function calls
 console.log(arrays.cut3(mixArr));
